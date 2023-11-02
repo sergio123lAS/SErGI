1a5216dcf1faf3148c79b27587b5e7b67cb0a68b
sergio123lAS <149724447+sergio123lAS@users.noreply.github.com>
2023-11-02 15:41:32 -0500

    Primera iteracion
    estoy creand la carpeta

---
+++ progra/sergio.py
@@ -0,0 +1,29 @@
+def registro():
+     asdasd
+     dasdasdasd
+def disponibles():
+     asdsadas
+     asdasdsa
+def vendidos():
+     asdasd
+     asdasdas
+     as
+
+while True:
+    print("Inmobiliaria")
+    print("1. Registrar")
+    print("2. Disponibles")
+    print("3. Vendidos")
+    opcion=input("opcion: ")
+    while opcion!="1" and opcion!="2" and opcion!="3":
+        opcion=input("opcion: ")
+    if opcion=="1":
+        registro()
+    if opcion=="1":
+        disponibles()
+    if opcion=="3":
+        vendidos()
+    print("Quiere salir?")
+    a=input()
+    if a=="si":
+        break
